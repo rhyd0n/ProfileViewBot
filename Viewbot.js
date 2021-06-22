3d504a8d8e69c28cf91af62613b4ea2c71d0aa5b
Egypt <80481493+BinaryEgypt@users.noreply.github.com>
2021-06-22 08:33:31 -0700

    Update Viewbot.js

--- Viewbot.js
+++ Viewbot.js
@@ -1,10 +1,9 @@
-const puppeteer = require('puppeteer');
-  
+const puppeteer = require('puppeteer'); //Thanks to this library
+// Using Headless chrome  
 async function bot() {
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto('https://github.com/UrUsernameh3re');
-    
-    await browser.close();
+    const page = await browser.newPage(); //opens page
+    await page.goto('https://github.com/UrUsernameh3re'); //put your username incthe specified location.
+    await browser.close(); //ends.
 }
-setInterval(bot,1000)
+setInterval(bot,100) //Re-doing the function multiple times.
