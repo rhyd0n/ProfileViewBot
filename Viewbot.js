a49d87a9815b0326a1934cc283bf3ab1989c082d
Egypt <80481493+BinaryEgypt@users.noreply.github.com>
2021-07-08 14:40:31 -0700

    Update Viewbot.js

--- Viewbot.js
+++ Viewbot.js
@@ -4,6 +4,6 @@ async function bot() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage(); //opens page
     await page.goto('https://github.com/UrUsernameh3re'); //put your username incthe specified location.
-    await browser.close(); //ends.
+    await browser.close(); //ends. 
 }
 setInterval(bot,100) //Re-doing the function multiple times.
